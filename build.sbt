f636cd02cc4a8d1518f8c4b1e5faf93d5b9138c5
Ilia Onishchenko <ilia_onishchenko@epam.com>
2017-08-28 17:53:44 +0300

    Updates counting algorithms

--- build.sbt
+++ build.sbt
@@ -14,6 +14,7 @@ libraryDependencies ++= Seq(
   "org.apache.hadoop" % "hadoop-common" % "2.7.3" % "provided",
   "org.apache.spark" % "spark-core_2.11" % "2.2.0" % "provided",
   "org.apache.spark" % "spark-streaming_2.11" % "2.2.0" % "provided",
+  "eu.bitwalker" % "UserAgentUtils" % "1.20",
   "org.apache.spark" % "spark-sql_2.11" % "2.2.0" % "provided"
 //  "org.scalatest" %% "scalatest_2.11" % "3.0.3" % "test"
 )
--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -1,14 +1,17 @@
 package com.example
 
+import eu.bitwalker.useragentutils.UserAgent
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
+
 import scala.util.{Failure, Success, Try}
 
 object Main {
 
   def main(args: Array[String]) = {
 
-    if (args.length != 2) {
-      println(s"Wrong number of arguments: ${args.length}")
+    if (args.length != 3) {
+      println(s"Wrong number of arguments: ${args.length}.\n Should be 3!")
     }
 
     val spark = SparkSession.builder
@@ -18,12 +21,22 @@ object Main {
     spark.sparkContext.hadoopConfiguration
       .set("mapreduce.input.fileinputformat.input.dir.recursive", "true")
 
-      spark.sparkContext.textFile(args(0))
-        .map(_.split(" "))
-        .map(arr => (arr(0), parseDouble(arr(9))))
-        .groupBy(_._1)
-        .mapValues(countStats)
-        .saveAsTextFile(args(1))
+    val file = spark.sparkContext.textFile(args(0)).map(_.split(" "))
+
+
+    file
+      .map(arr => (arr(0), parseDouble(arr(9))))
+      .groupBy(_._1)
+      .mapValues(countStats)
+      .saveAsTextFile(args(1))
+
+    file
+      .map(arr => detectBrowser(arr(11)))
+      .groupBy((browser: String) => browser)
+      .mapValues(iter => iter.size)
+//      .map(browsers => (browsers._1, browsers._2.size))
+      .saveAsTextFile(args(2))
+
   }
 
   def parseDouble(str: String): Double = Try(str.toDouble) match {
@@ -36,5 +49,10 @@ object Main {
     val sum = byteList.sum
     val avg = sum / byteList.size
     s"$avg, $sum"
+  }
+
+  def detectBrowser(str: String): String = {
+    val browser = UserAgent.parseUserAgentString(str).getBrowser
+    browser.getName
   }
 }
